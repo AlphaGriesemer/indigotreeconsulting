3d4bb60ec8813b245b8e7002c301c5856f706e88
brianzhu <bzhu@parktudor.org>
2021-02-20 17:52:52 -0500

    added html, css, js, and images to the django project

---
+++ itc/framework/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name="index"),
+    path('services/',views.ServiceView.as_view(), name="services"),
+    path('contact/', views.ContactView.as_view(), name="contact")
+]
--- itc/framework/views.py
+++ itc/framework/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
 # Create your views here.
+class IndexView(TemplateView):
+    template_name = "framework/homepage.html"
+
+
+class ServiceView(TemplateView):
+    template_name = "framework/services.html"
+
+
+class ContactView(TemplateView):
+    template_name = "framework/contact.html"
+
