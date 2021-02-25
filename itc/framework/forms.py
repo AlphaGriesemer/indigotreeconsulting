5dbb5eb1fd7f3f34a9d94f923c72508370d662ec
Alex Griesemer <47705026+AlphaGriesemer@users.noreply.github.com>
2021-02-25 14:37:42 -0500

    contact page changes

--- itc/framework/forms.py
+++ itc/framework/forms.py
@@ -5,14 +5,13 @@ from itc.settings import EMAIL_HOST_USER
 
 class ContactForm (Form):
     first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
     email = forms.CharField(required=True)
     comment = forms.CharField(required=True, widget=forms.Textarea)
 
     def send_email(self):
         clean_data = self.cleaned_data
         send_mail(
-            subject=f"Email from {clean_data.get('first_name')} {clean_data.get('last_name')} ({clean_data.get('email')})",
+            subject=f"Email from {clean_data.get('first_name')} ({clean_data.get('email')})",
             message=clean_data.get('comment'),
             from_email=EMAIL_HOST_USER,
             recipient_list=[EMAIL_HOST_USER]
