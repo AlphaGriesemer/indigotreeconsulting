6b1bc75c052df2c3844361b100343461c3f43a07
Alex Griesemer <47705026+AlphaGriesemer@users.noreply.github.com>
2021-03-02 09:56:42 -0500

    contact changes

--- itc/framework/forms.py
+++ itc/framework/forms.py
@@ -4,9 +4,9 @@ from django.core.mail import send_mail
 from itc.settings import EMAIL_HOST_USER
 
 class ContactForm (Form):
-    first_name = forms.CharField(required=True)
-    email = forms.CharField(required=True)
-    comment = forms.CharField(required=True, widget=forms.Textarea)
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'id' : 'firstName', 'placeholder' : 'Name *'}))
+    email = forms.CharField(required=True, widget=forms.TextInput(attrs={'id' : 'email', 'placeholder' : 'Email *'}) )
+    comment = forms.CharField(required=True, widget=forms.Textarea(attrs={'id' : 'message', 'placeholder' : 'Message *'}))
 
     def send_email(self):
         clean_data = self.cleaned_data
