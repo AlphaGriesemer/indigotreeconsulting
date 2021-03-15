ff989883b48436577b8d4c1a6707f3574aad6b2e
Alex Griesemer <47705026+AlphaGriesemer@users.noreply.github.com>
2021-03-15 10:34:27 -0400

    contact update

--- itc/framework/forms.py
+++ itc/framework/forms.py
@@ -4,9 +4,9 @@ from django.core.mail import send_mail
 from itc.settings import EMAIL_HOST_USER
 
 class ContactForm (Form):
-    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'id' : 'firstName', 'placeholder' : 'Name *'}))
-    email = forms.CharField(required=True, widget=forms.TextInput(attrs={'id' : 'email', 'placeholder' : 'Email *'}) )
-    comment = forms.CharField(required=True, widget=forms.Textarea(attrs={'id' : 'message', 'placeholder' : 'Message *'}))
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'id' : 'firstName', 'placeholder' : 'Name *', 'style' : 'width: 22vw; padding: 16px; margin-top: 10px;'}))
+    email = forms.CharField(required=True, widget=forms.TextInput(attrs={'id' : 'email', 'placeholder' : 'Email *', 'style' : 'width: 22vw; padding: 16px; margin-top: 10px;'}) )
+    comment = forms.CharField(required=True, widget=forms.Textarea(attrs={'id' : 'message', 'placeholder' : 'Message *', 'style' : 'width: 45vw;'}))
 
     def send_email(self):
         clean_data = self.cleaned_data
